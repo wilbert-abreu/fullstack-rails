9a1285339b49bc28f638f9977a4f08ffb1f4bf7a
Wilbert Abreu <abreu.wilbert@gmail.com>
2016-11-26 16:01:39 +0000

    Completed up to Heroku

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+    root to: 'pages#home'
+    get 'about', to: 'pages#about'
+    resources :contacts, only: :create
+    get 'contact-us', to: 'contacts#new', as: 'new_contact'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
