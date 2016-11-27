96d4bbc0fedc708e8dbc6bc160baa69a0dc81834
Wilbert Abreu <abreu.wilbert@gmail.com>
2016-11-27 19:27:47 +0000

    Added code comment

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,14 +1,25 @@
 class ContactsController < ApplicationController
+    
+    # GET request to /contact-us
+    # Show new contact form
     def new
     @contact = Contact.new
     end
+    
+    # POST request /contacts
     def create
+      # Mass assignment of form fields into Contact object
       @contact = Contact.new(contact_params)
+      # Save the Contact object to the database
       if @contact.save
+         # Store form fields via parmeters, into variables
          name = params[:contact][:name]
          email = params[:contact][:email]
          body = params[:contact][:comments]
+         # Plug variables into Contact Mailer 
+         # email method and send email
          ContactMailer.contact_email(name, email, body).deliver
+         # Store success message in flash hash
          flash[:success] = "Message sent."
          redirect_to new_contact_path
       else
